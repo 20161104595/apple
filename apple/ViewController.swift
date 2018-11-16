e653cf135f29291b9e44170733b2fb0dc2f9b023
20161104595 <a20161104595@mac25.local>
2018-11-16 17:36:58 +0800

    ..
    ..

--- apple/ViewController.swift
+++ apple/ViewController.swift
@@ -214,7 +214,16 @@ class ViewController: UIViewController {
             d = x * c
         }else if number == 4{
             d = x/(c)
-
+        }else{
+            d=1000
+        }
+        player2.text = String(c)
+        if judge == 1{
+           player.text = String(format:"%f",d)
+        }else{
+           player.text = String(format:"%.10f",d)
+        }
+        if c == 0 && number == 4{
             player.text = "错误"
         }
         re = 1
